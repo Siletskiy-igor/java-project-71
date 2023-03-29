91ed4ecdb0c8cebe3b190fabb739bc87ca34573c
Igor <82278294+Siletskiy-igor@users.noreply.github.com>
2023-03-30 00:00:15 +0300

    Изменения в app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,14 +4,46 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command (name = "gendiff", description = "Compares two configuration files and shows a difference.")
-public class App {
-    @Option (names = {"-h", "--help"}, description = "Show this help message and exit.")
-    private boolean helpRequested = false;
-    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    boolean version;
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
 
+@Command (name = "gendiff", description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private File file1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private File file2;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this @|fg(blue) help|@ message")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]", paramLabel = "format")
+    String format;
+
+    @Override
+    public Integer call() throws Exception {
+//        byte[] fileContents = Files.readAllBytes(help.toPath());
+//        byte[] digest = MessageDigest.getInstance(version).digest(fileContents);
+//        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
     public static void main(String[] args) {
-        new CommandLine(new App()).usage(System.out);
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
     }
 }
