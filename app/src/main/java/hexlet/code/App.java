fbef13d0a55f3a920457b41f151449743c98f168
Igor <82278294+Siletskiy-igor@users.noreply.github.com>
2023-03-29 19:54:42 +0300

    добавил picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,17 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command (name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option (names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    boolean version;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).usage(System.out);
     }
 }
