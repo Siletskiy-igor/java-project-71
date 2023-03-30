b97460b724e79b0a3346cc7f9c06f027515bf8af
Igor <82278294+Siletskiy-igor@users.noreply.github.com>
2023-03-30 20:53:57 +0300

    выполнил 5 задание

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,19 +5,16 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 @Command (name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
     @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
-    private File file1;
+    private String file1;
 
     @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
-    private File file2;
+    private String file2;
 
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
     boolean versionInfoRequested;
@@ -30,20 +27,12 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-//        byte[] fileContents = Files.readAllBytes(help.toPath());
-//        byte[] digest = MessageDigest.getInstance(version).digest(fileContents);
-//        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        System.out.println(Differ.generate(file1, file2));
         return 0;
     }
-    public static void main(String[] args) {
-        CommandLine commandLine = new CommandLine(new App());
-        commandLine.parseArgs(args);
-        if (commandLine.isUsageHelpRequested()) {
-            commandLine.usage(System.out);
-            return;
-        } else if (commandLine.isVersionHelpRequested()) {
-            commandLine.printVersionHelp(System.out);
-            return;
-        }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,4 +1,66 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+
 public class Differ {
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+
+        String content1 = getContent(filePath1);
+        String content2 = getContent(filePath2);
+
+        Map<String, Object> data1 = parce(content1);
+        Map<String, Object> data2 = parce(content2);
+
+        StringBuilder stringBuilder = new StringBuilder("{\n");
+        for (Map.Entry<String, Object> e: data1.entrySet()) {
+            String data1Key = e.getKey();
+            Object data1Value = e.getValue();
+            if(data2.containsKey(data1Key) && data2.get(data1Key).equals(data1Value)) {
+                stringBuilder.append("  ").append(data1Key).append(": ").append(data1Value).append("\n");
+            } else if (!data2.containsKey(data1Key)) {
+                stringBuilder.append("- ").append(data1Key).append(": ").append(data1Value).append("\n");
+            } else if (data2.containsKey(data1Key) && !data2.get(data1Key).equals(data1Value)){
+                stringBuilder.append("- ").append(data1Key).append(": ").append(data1Value).append("\n").append("+ ").append(data1Key).append(": ").append(data2.get(data1Key)).append("\n");
+            }
+        }
+        for (String e: data2.keySet()) {
+            if (!data1.containsKey(e)) {
+                stringBuilder.append("+ ").append(e).append(": ").append(data2.get(e)).append("\n");
+            }
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
+
+    }
+    public static String getContent(String possiblePath) throws Exception {
+
+        Path originalPath = Paths.get(possiblePath);
+        Path absolutePath = originalPath.toAbsolutePath().normalize();
+
+        if (!Files.exists(absolutePath)) {
+            throw new Exception("File '" + absolutePath + "' does not exist");
+        }
+
+        return Files.readString(absolutePath);
+    }
+
+
+    public static TreeMap<String, Object> parce(String content) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(content, new TypeReference<>() {});
+    }
+
+
 }
