52f5ad1f8832a506793d5d6f40f688708bd49d00
Igor <82278294+Siletskiy-igor@users.noreply.github.com>
2023-04-01 21:57:25 +0300

    something

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,19 +9,28 @@ import java.util.concurrent.Callable;
 @Command (name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-    @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    @CommandLine.Parameters(index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1")
     private String file1;
 
-    @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    @CommandLine.Parameters(index = "1",
+            description = "path to second file",
+            paramLabel = "filepath2")
     private String file2;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    @Option(names = {"-V", "--version"},
+            versionHelp = true,
+            description = "display version info")
     boolean versionInfoRequested;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this @|fg(blue) help|@ message")
+    @Option(names = {"-h", "--help"},
+            usageHelp = true,
+            description = "display this @|fg(blue) help|@ message")
     boolean usageHelpRequested;
 
-    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+    @Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
             description = "output format [default: ${DEFAULT-VALUE}]",
             paramLabel = "format")
     String format;
